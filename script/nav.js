ff87f9ce19543f27b3275f2608c84947dd124635
nabin kharel <nabinkharel142@gmail.com>
2023-10-11 21:32:41 +0545

    feat: log check

--- script/nav.js
+++ script/nav.js
@@ -1,5 +1,6 @@
 const menu = document.getElementById("menu");
 const sideNav = document.getElementsByClassName("m-nav");
+const log = document.getElementsByClassName("log");
 
 let show = false;
 
@@ -7,3 +8,25 @@ menu.addEventListener("click", () => {
   show = !show;
   sideNav[0].style.width = show ? "320px" : "0px";
 });
+
+let user = JSON.parse(localStorage?.getItem("user"));
+
+if (user.U_id) {
+  for (i in log) {
+    i.innerHtml = "<li><button>logout</button></li>";
+  }
+} else {
+  for (i in log) {
+    i.innerHtml = `<li>
+  <a style="color: black" href="./for-company.html">For Company</a>
+</li>
+<li>
+  <a style="color: black" href="./login.html">Log in</a>
+</li>
+<li>
+  <button class="btn">
+    <a style="color: white" href="./sign-up.html">Sign up</a>
+  </button>
+</li>`;
+  }
+}
